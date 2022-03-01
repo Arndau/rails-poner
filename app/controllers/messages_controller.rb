1394ac4b533f38760e7649da5d2dc6069556e326
Amaury Vallet <amaury.vallet@gmail.com>
2022-03-01 15:40:12 +0100

    pundit en +

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,8 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:destroy, :show]
 
   def index
     @messages = policy_scope(Message)
-    @messages = Message.all
 
     # the `geocoded` scope filters only flats with coordinates (latitude & longitude)
     @markers = @flats.geocoded.map do |flat|
@@ -12,4 +12,26 @@ class MessagesController < ApplicationController
       }
     end
   end
+
+  def show
+  end 
+
+  def new
+    @message = Message.new
+    authorize @message
+  end
+
+  def destroy
+    @message.destroy
+  end
+
+
+
+  private
+
+  def set_message
+    @message = Message.find(params[:id])
+    authorize @message
+  end
+
 end
