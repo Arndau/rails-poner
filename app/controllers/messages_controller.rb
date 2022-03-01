9687f9adf81e7c6976736a163af0bb078832a036
guillaume levasseur <levasseurguillaume91@gmail.com>
2022-03-01 16:02:16 +0100

    du sal

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,14 +2,13 @@ class MessagesController < ApplicationController
 
   def index
     @messages = policy_scope(Message)
-    @messages = Message.all
 
     # the `geocoded` scope filters only flats with coordinates (latitude & longitude)
-    @markers = @flats.geocoded.map do |flat|
-      {
-        lat: flat.latitude,
-        lng: flat.longitude
-      }
-    end
+    # @markers = @messages.geocoded.map do |message|
+    #   {
+    #     lat: message.latitude,
+    #     lng: message.longitude
+    #   }
+    # end
   end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,7 @@
 class Message < ApplicationRecord
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
+
   belongs_to :user
 
   validates :hint, presence: true
