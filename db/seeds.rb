dc93daefd6278c84e864c54d132cccac15db3e96
Gorrog87 <gourguesmaxime@gmail.com>
2022-03-01 12:10:27 +0100

    first user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Message.destroy_all
+MessageUser.destroy_all
+
+
+user1 = User.create!(email: "max@test.com", password: "123456", first_name:"Max", last_name: "Gourgues")
+#user2 = User.create!(email: "test@gmail.com", password: "123456")
+
+
+#message1 = Message.create!(hint: "pettie surprise pour toi...", address: "16 villa Gaudelet", lat: "48.865131", long: "2.379999")
+#message2 = Message.create!(hint: "ton restaurant préféré", address: "110 rue Oberkampf", lat: "48.866667", long: "2.333333")
