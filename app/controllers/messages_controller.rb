cfb1a57489a6e23622b9948454c1823ca192deb3
guillaume levasseur <levasseurguillaume91@gmail.com>
2022-03-01 12:44:52 +0100

    to infinity and beyond

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,7 @@
 class MessagesController < ApplicationController
 
   def index
-
+    @messages = policy_scope(Message)
+    @messages = Message.all
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Message.destroy_all
+MessageUser.destroy_all
+
+
+user1 = User.create!(email: "max@test.com", password: "123456", first_name:"Max", last_name: "Gourgues")
