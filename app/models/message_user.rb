d159c344b18ec747a5f0d2f26c960e914fc8c1f0
Gorrog87 <gourguesmaxime@gmail.com>
2022-03-01 15:08:29 +0100

    seeds terminées

--- app/models/message_user.rb
+++ app/models/message_user.rb
@@ -1,7 +1,4 @@
 class MessageUser < ApplicationRecord
   belongs_to :message
   belongs_to :user
-
-  validates :unlocked, presence: true
-  validates :read, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -15,4 +15,11 @@ user3 = User.create!(email: "Arnaud@gmail.com", password: "123456", first_name:"
 user4 = User.create!(email: "maxime@gmail.com", password: "123456", first_name:"maxime", last_name: "Gourgues")
 
 message1 = Message.create!(hint: "pettie surprise pour toi...", address: "16 villa Gaudelet", latitude: "48.865131", longitude: "2.379999", user: user1)
-#message2 = Message.create!(hint: "ton restaurant préféré", address: "110 rue Oberkampf", lat: "48.866667", long: "2.333333")
+message2 = Message.create!(hint: "ton restaurant préféré", address: "110 rue Oberkampf", latitude: "48.8659664", longitude: "2.3790391", user: user2)
+message3 = Message.create!(hint:"test test", address: "rue princesse", latitude: "48.8527104", longitude: "2.3344514", user: user3)
+message4 = Message.create!(hint: "Bel endroit", address: "14 rue de la roquette", latitude: "48.8537039", longitude: "2.3708532", user: user4)
+
+message_user1 = MessageUser.create!(unlocked: true, read: true, message: message1, user: user2)
+message_user2 = MessageUser.create!(unlocked: false, read: false, message: message2, user: user3)
+message_user3 = MessageUser.create!(unlocked: true, read: false, message: message3, user: user4)
+message_user4 = MessageUser.create!(unlocked: false, read: true, message: message4, user: user1)
