5ae36a5f62f572ef99775b0cec9c7e11aa26a891
Gorrog87 <gourguesmaxime@gmail.com>
2022-03-01 17:53:29 +0100

    seed étoffée Users-controller aborted

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,8 +20,14 @@ message1 = Message.create!(hint: "pettie surprise pour toi...", address: "16 vil
 message2 = Message.create!(hint: "ton restaurant préféré", address: "110 rue Oberkampf", latitude: "48.8659664", longitude: "2.3790391", user: user2)
 message3 = Message.create!(hint:"test test", address: "rue princesse", latitude: "48.8527104", longitude: "2.3344514", user: user3)
 message4 = Message.create!(hint: "Bel endroit", address: "14 rue de la roquette", latitude: "48.8537039", longitude: "2.3708532", user: user4)
+message5 = Message.create!(hint: "Some wagoners friends", address: "Le Perchoir, 14 Rue Crespin du Gast, 75011 Paris, France", latitude: "48.8654176", longitude: "2.3819734", user: user3)
+message6 = Message.create!(hint: "Le lieu de tous nos débriefs !!", address: "105 Rue Oberkampf", latitude: "48.86587142944336", longitude: "2.377452850341797", user: user3)
+
 
 message_user1 = MessageUser.create!(unlocked: true, read: true, message: message1, user: user2)
+message_user1 = MessageUser.create!(unlocked: false, read: true, message: message4, user: user3)
+message_user1 = MessageUser.create!(unlocked: false, read: true, message: message5, user: user3)
+message_user1 = MessageUser.create!(unlocked: false, read: true, message: message6, user: user3)
 message_user2 = MessageUser.create!(unlocked: false, read: false, message: message2, user: user3)
 message_user3 = MessageUser.create!(unlocked: true, read: false, message: message3, user: user4)
 message_user4 = MessageUser.create!(unlocked: false, read: true, message: message4, user: user1)
