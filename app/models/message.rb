f832ce1349d355423d6d92e75692ec75cc78c8ed
Arnaud Le Garrec <legarrecarnaud.92@gmail.com>
2022-03-02 15:53:14 +0100

    implémentation de la map & fonctionnel

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,6 +3,7 @@ class Message < ApplicationRecord
   after_validation :geocode, if: :will_save_change_to_address?
 
   belongs_to :user
+  has_many :message_users
 
   validates :hint, presence: true
   validates :address, presence: true
